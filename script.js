37d06d1c21380d7ebbe6713e12ddc6f9507214fb
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-10-24 11:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+
+    if (id === "C") {
+      display.value = "";
+    } else if (id === "back") {
+      display.value = display.value.slice(0, -1);
+    } else if (id === "equal") {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = "Error";
+      }
+    } else {
+      if (id === "mul") display.value += "*";
+      else if (id === "minus") display.value += "-";
+      else display.value += btn.innerText;
+    }
+  });
+});
