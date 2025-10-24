f7d710e9346f2b80cf10fbace99d6d35d6c71969
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-10-24 12:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
@@ -7,19 +6,35 @@ buttons.forEach((btn) => {
     const id = btn.id;
 
     if (id === "C") {
+      // Clear all
       display.value = "";
     } else if (id === "back") {
+      // Clear one character
       display.value = display.value.slice(0, -1);
     } else if (id === "equal") {
+      // Evaluate expression
       try {
         display.value = eval(display.value);
       } catch {
         display.value = "Error";
       }
+    } else if (id === "divi") {
+      display.value += "/";
+    } else if (id === "plus") {
+      display.value += "+";
+    } else if (id === "-") {
+      display.value += "-";
+    } else if (id === "*") {
+      display.value += "*";
+    } else if (id === "op") {
+      display.value += "(";
+    } else if (id === "cl") {
+      display.value += ")";
+    } else if (id === "dot") {
+      display.value += ".";
     } else {
-      if (id === "mul") display.value += "*";
-      else if (id === "minus") display.value += "-";
-      else display.value += btn.innerText;
+      // For all number buttons
+      display.value += id;
     }
   });
 });
